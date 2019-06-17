c5117fd27fcd6ec0092126d9b2f81af74b41e368
Rosaria De Biasio <debiasiorosaria@gmail.com>
2019-06-17 14:28:18 +0200

    add elements via DOM

--- experiments/todos-app/src/main.js
+++ experiments/todos-app/src/main.js
@@ -1,1 +1,38 @@
+const todos = [{
+  text: 'Fare la spesa',
+  completed: true
+}, {
+  text: 'Comprare il vestito',
+  completed: false
+}, {
+  text: 'Fare sport',
+  completed: false
+}, {
+  text: 'Studiare',
+  completed: true
+}, {
+  text: 'Fare pulizie',
+  completed: true
+}, {
+  text: 'Pittare Casa',
+  completed: false
+}, {
+  text: 'Prendere il treno',
+  completed: false
+}]
+
+const incompleteTodos = todos.filter((todo) => {
+  return !todo.completed
+})
+
+const newTitle = document.createElement('h3');
+newTitle.textContent = `Hai ${incompleteTodos.length} attività incomplete`;
+document.querySelector('body').appendChild(newTitle);
+
+todos.forEach((todo) => {
+  const newParagraph = document.createElement('p');
+  newParagraph.textContent = todo.text;
+  document.querySelector('body').appendChild(newParagraph);
+})
+
 
