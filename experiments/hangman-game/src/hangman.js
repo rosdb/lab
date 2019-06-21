fc5df87d5f7214f5abb2e3b2088d427a5707d7d6
Rosaria De Biasio <debiasiorosaria@gmail.com>
2019-06-21 14:32:26 +0200

    refactory and display on browser

---
+++ experiments/hangman-game/src/hangman.js
@@ -0,0 +1,34 @@
+const Hangman = function (word, remainingGuesses) {
+  this.word = word.toLowerCase().split('');
+  this.remainingGuesses = remainingGuesses;
+  this.guessedLetters = [];
+};
+
+Hangman.prototype.getPuzzle = function () {
+  let puzzle = '';
+
+  this.word.forEach(letter => {
+    if (this.guessedLetters.includes(letter) || letter === ' ') {
+      puzzle += letter;
+    } else {
+      puzzle += '*';
+    }
+  });
+
+  return puzzle;
+};
+
+Hangman.prototype.makeGuess = function (guess) {
+  // eslint-disable-next-line no-param-reassign
+  guess = guess.toLowerCase();
+  const isUnique = !this.guessedLetters.includes(guess);
+  const isBadGuess = !this.word.includes(guess);
+
+  if (isUnique) {
+    this.guessedLetters.push(guess);
+  }
+
+  if (isUnique && isBadGuess) {
+    this.remainingGuesses = this.remainingGuesses - 1;
+  }
+};
