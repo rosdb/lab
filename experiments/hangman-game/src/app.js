1d09adb509378a3a1d916de7e4d9a03ac26e0511
Rosaria De Biasio <debiasiorosaria@gmail.com>
2019-06-22 19:22:43 +0200

    getters and setters

--- experiments/hangman-game/src/app.js
+++ experiments/hangman-game/src/app.js
@@ -2,13 +2,13 @@ const puzzleEl = document.querySelector('#puzzle');
 const guessesEl = document.querySelector('#guesses');
 const game1 = new Hangman('Cat', 2);
 
-puzzleEl.textContent = game1.getPuzzle();
-guessesEl.textContent = game1.statusMessage();
+puzzleEl.textContent = game1.puzzle;
+guessesEl.textContent = game1.statusMessage;
 
 
 window.addEventListener('keypress', evt => {
   const guess = String.fromCharCode(evt.charCode);
   game1.makeGuess(guess);
-  puzzleEl.textContent = game1.getPuzzle();
-  guessesEl.textContent = game1.statusMessage();
+  puzzleEl.textContent = game1.puzzle;
+  guessesEl.textContent = game1.statusMessage;
 });
--- experiments/hangman-game/src/hangman.js
+++ experiments/hangman-game/src/hangman.js
@@ -19,7 +19,7 @@ class Hangman {
     }
   }
 
-  statusMessage() {
+  get statusMessage() {
     if (this.status === 'playing') {
       return `Guesses left: ${this.remainingGuesses}`;
     } else if (this.status === 'failed') {
@@ -29,7 +29,7 @@ class Hangman {
     }
   }
 
-  getPuzzle() {
+  get puzzle() {
     let puzzle = '';
 
     this.word.forEach(letter => {
