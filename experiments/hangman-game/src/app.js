376dd456636419b9f0762b1bdc05d0a08fa086a8
Rosaria De Biasio <debiasiorosaria@gmail.com>
2019-06-24 11:13:28 +0200

    HTTP Request

--- experiments/hangman-game/src/app.js
+++ experiments/hangman-game/src/app.js
@@ -12,3 +12,39 @@ window.addEventListener('keypress', evt => {
   puzzleEl.textContent = game1.puzzle;
   guessesEl.textContent = game1.statusMessage;
 });
+
+//HTTP request
+const request = new XMLHttpRequest();
+
+request.addEventListener('readystatechange', evt => {
+  if (evt.target.readyState === 4 && evt.target.status === 200) {
+    const data = JSON.parse(evt.target.responseText);
+    // eslint-disable-next-line no-console
+    console.log(data);
+  } else if (evt.target.readyState === 4) {
+    // eslint-disable-next-line no-console
+    console.log('error');
+  }
+});
+
+request.open('GET', 'http://puzzle.mead.io/puzzle');
+request.send();
+
+
+const countryRequest = new XMLHttpRequest();
+
+countryRequest.addEventListener('readystatechange', evt => {
+  if (evt.target.readyState === 4 && evt.target.status === 200) {
+    const data = JSON.parse(evt.target.responseText);
+    const countryCode = 'IT';
+    const country = data.find(el => el.alpha2Code === countryCode);
+    // eslint-disable-next-line no-console
+    console.log(country.name);
+  } else if (evt.target.readyState === 4) {
+    // eslint-disable-next-line no-console
+    console.log('error');
+  }
+});
+
+countryRequest.open('GET', 'http://restcountries.eu/rest/v2/all');
+countryRequest.send();
