cc4ba3f2855132b2e932bca4a1b8286605bdc8db
Rosaria De Biasio <debiasiorosaria@gmail.com>
2019-06-25 16:41:39 +0200

    implement async/await

--- experiments/hangman-game/src/requests.js
+++ experiments/hangman-game/src/requests.js
@@ -1,30 +1,34 @@
-/* eslint-disable no-unused-vars */
-const getPuzzle = wordCount => fetch(`http://puzzle.mead.io/puzzle?wordCount=${wordCount}`).then(response => {
+
+const getPuzzle = async wordCount => {
+  const response = await fetch(`http://puzzle.mead.io/puzzle?wordCount=${wordCount}`);
   if (response.status === 200) {
-    return response.json();
+    const data = await response.json();
+    return data.puzzle;
   } else {
     throw new Error('Unable to fetch puzzle');
   }
-}).then(data => data.puzzle);
+};
 
 
 
-const getCountry = code => fetch('http://restcountries.eu/rest/v2/all').then(response => {
+const getCountry = async code => {
+  const response = await fetch('http://restcountries.eu/rest/v2/all');
   if (response.status === 200) {
-    return response.json();
+    const data = await response.json();
+    const country = await data.find(el => el.alpha2Code === code);
+    return country.name;
   } else {
     throw new Error('Unable to fetch data');
   }
-}).then(data => {
-  const country = data.find(el => el.alpha2Code === code);
-  return country.name;
-});
+};
 
 
-const getLocation = () => fetch('https://ipinfo.io/json?token=721f4bba8f9ab6').then(response => {
+const getLocation = async () => {
+  const response = await fetch('https://ipinfo.io/json?token=721f4bba8f9ab6');
   if (response.status === 200) {
-    return response.json();
+    const data = response.json();
+    return data;
   } else {
     throw new Error('Unable to fetch location');
   }
-}).then(data => data);
+};
