1bd62147c52d7042a2c3d5c835127b1d1b768963
Rosaria De Biasio <debiasiorosaria@gmail.com>
2019-06-25 17:16:54 +0200

    adding reset button for start new game

--- experiments/hangman-game/src/app.js
+++ experiments/hangman-game/src/app.js
@@ -1,32 +1,43 @@
 /* eslint-disable no-console */
 const puzzleEl = document.querySelector('#puzzle');
 const guessesEl = document.querySelector('#guesses');
-const game1 = new Hangman('Cat', 2);
-
-puzzleEl.textContent = game1.puzzle;
-guessesEl.textContent = game1.statusMessage;
+let game1;
 
 
 window.addEventListener('keypress', evt => {
   const guess = String.fromCharCode(evt.charCode);
   game1.makeGuess(guess);
+  render();
+});
+
+const render = () => {
   puzzleEl.textContent = game1.puzzle;
   guessesEl.textContent = game1.statusMessage;
-});
+};
 
-//HTTP requests
+const startGame = async () => {
+  const puzzle = await getPuzzle('2');
+  game1 = new Hangman(puzzle, 5);
+  render();
+};
 
-getPuzzle('2').then(puzzle => {
-  console.log(puzzle);
-}).catch(error => {
-  console.log(error);
-});
+document.querySelector('#reset').addEventListener('click', startGame);
+
+startGame();
+
+// getPuzzle('2').then(puzzle => {
+//   console.log(puzzle);
+// }).catch(error => {
+//   console.log(error);
+// });
 
 
-getCurrentCountry().then(country => {
-  console.log(country);
-}).catch(error => {
-  console.log(error);
-});
+// getCurrentCountry().then(country => {
+//   console.log(country);
+// }).catch(error => {
+//   console.log(error);
+// });
+
+
 
 
