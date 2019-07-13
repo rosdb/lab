1f328b83c3cad7577b84dadcd9dc7b00207ac539
Rosaria De Biasio <debiasiorosaria@gmail.com>
2019-07-13 19:01:17 +0200

    build react todo list

---
+++ experiments/react-todolist/src/Todo.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+import './Todo.css';
+
+class Todo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isEditing: false,
+      task: this.props.task
+    };
+    this.handleRemove = this.handleRemove.bind(this);
+    this.toggleOnForm = this.toggleOnForm.bind(this);
+    this.handleUpdate = this.handleUpdate.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleToggle = this.handleToggle.bind(this);
+  }
+  handleRemove() {
+    this.props.removeTodo(this.props.id)
+  }
+
+  toggleOnForm() {
+    this.setState({ isEditing: !this.state.isEditing });
+  }
+
+  handleUpdate(evt) {
+    evt.preventDefault();
+    this.props.updateTodo(this.props.id, this.state.task);
+    this.setState({ isEditing: false });
+  }
+
+  handleChange(evt) {
+    this.setState({
+      [evt.target.name]: evt.target.value
+    })
+  }
+
+  handleToggle(evt) {
+    this.props.toggleTodo(this.props.id);
+  }
+
+  render() {
+    let result;
+    if (this.state.isEditing) {
+      result = (
+        <div className="Todo">
+          <form className="Todo-edit-form" onSubmit={this.handleUpdate}>
+            <input type='text' value={this.state.task} name="task" onChange={this.handleChange} />
+            <button>Save</button>
+          </form>
+        </div>
+      );
+    } else {
+      result = (
+        <div className="Todo">
+          <li className={this.props.completed ? 'Todo-task completed' : 'Todo-task'} onClick={this.handleToggle}>{this.props.task}</li>
+          <div className="Todo-buttons">
+            <button onClick={this.toggleOnForm}>
+              <i class='fas fa-pen'></i>
+            </button>
+            <button onClick={this.handleRemove}>
+              <i class='fas fa-trash'></i>
+            </button>
+          </div>
+        </div>
+      );
+    }
+    return result;
+  }
+}
+
+export default Todo;
