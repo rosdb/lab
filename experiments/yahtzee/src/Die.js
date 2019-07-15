400204cd45905ac98b81948d77ee2147ec7246a5
Rosaria De Biasio <debiasiorosaria@gmail.com>
2019-07-15 11:47:46 +0200

    build and fix yahtzee

---
+++ experiments/yahtzee/src/Die.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import "./Die.css";
+
+class Die extends Component {
+  static defaultProps = {
+    numberWords: ["one", "two", "three", "four", "five", "six"],
+    val: 1
+  }
+  render() {
+    const { idx, numberWords, locked, val, disabled, rolling } = this.props;
+    let classes = `Die fas fa-dice-${numberWords[val - 1]} fa-5x `;
+    if (locked) classes += "Die-locked";
+    if (rolling) classes += "Die-rolling";
+    return (
+      <i
+        className={classes}
+        onClick={() => this.props.handleClick(idx)}
+        disabled={disabled}
+      />
+    );
+  }
+}
+
+export default Die;
