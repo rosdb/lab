579789dc9fe24efa3a5553bd00b1898eb634681b
Rosaria De Biasio <debiasiorosaria@gmail.com>
2019-08-06 17:44:06 +0200

    delete formattingList

--- experiments/sass-natours/scripts/clean-dist.js
+++ experiments/sass-natours/scripts/clean-dist.js
@@ -3,18 +3,6 @@ const shell = require('shelljs');
 
 const DIST_DIR = path.resolve(__dirname, '../', 'dist');
 
-// --- Helpers
-function formattingList(files) {
-  if (!Array.isArray(files)) {
-    shell.exit(1);
-    return;
-  }
-
-  return files.length >= 1
-    ? files.join('\n').toString()
-    : 'Specifies a list of file to delete.';
-}
-
 // --- Cleaner
 function clean(targets) {
   if (!Array.isArray(targets)) {
@@ -23,7 +11,6 @@ function clean(targets) {
   }
 
   shell.echo('Clean project...');
-  shell.echo(formattingList(targets));
   shell.rm('-r', targets);
 }
 
