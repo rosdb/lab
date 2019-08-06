6e90bce18ead2f3b401272a9fd82a328e2efb0d2
Rosaria De Biasio <debiasiorosaria@gmail.com>
2019-08-06 17:03:12 +0200

    add clean on setup.js

--- experiments/sass-natours/scripts/setup.js
+++ experiments/sass-natours/scripts/setup.js
@@ -4,6 +4,30 @@ const shell = require('shelljs');
 const DIST_DIR = path.resolve(__dirname, '../', 'dist');
 const PUBLIC_DIR = path.resolve(__dirname, '../', 'public');
 
+// --- Helpers
+function formattingList(files) {
+  if (!Array.isArray(files)) {
+    shell.exit(1);
+    return;
+  }
+
+  return files.length >= 1
+    ? files.join('\n').toString()
+    : 'Specifies a list of file to delete.';
+}
+
+// --- Cleaner
+function clean(targets) {
+  if (!Array.isArray(targets)) {
+    shell.exit(1);
+    return;
+  }
+
+  shell.echo('Clean project...');
+  shell.echo(formattingList(targets));
+  shell.rm('-r', targets);
+}
+
 // --- Copy
 function copy(from, target) {
   if (typeof from !== 'string' && typeof target !== 'string') {
@@ -19,6 +43,7 @@ function copy(from, target) {
 
 // --- Program
 (function run() {
+  clean([DIST_DIR]);
   copy(PUBLIC_DIR, DIST_DIR);
   shell.exit(0);
 })();
