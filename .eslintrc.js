1db56d065a88f274ad85beded9735e9158d29b93
Rosaria De Biasio <debiasiorosaria@gmail.com>
2019-08-06 12:13:45 +0200

    fix error eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,7 @@ module.exports = {
     'import/no-unresolved': 0,
     'import/no-extraneous-dependencies': 0,
     'class-methods-use-this': 0,
+    'guard-for-in': 0,
     'no-use-before-define': 0,
     'no-shadow': 0,
     'no-restricted-syntax': 0,
