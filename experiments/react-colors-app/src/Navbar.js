fa1ec48d77551afd96c0e7cf8e98dd3ce3485c6d
Rosaria De Biasio <debiasiorosaria@gmail.com>
2019-08-08 12:16:39 +0200

    add Snackbar and style Select

--- experiments/react-colors-app/src/Navbar.js
+++ experiments/react-colors-app/src/Navbar.js
@@ -1,6 +1,9 @@
 import React, {Component} from 'react';
 import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
+import Snackbar from '@material-ui/core/Snackbar';
+import CloseIcon from '@material-ui/icons/Close';
+import IconButton from '@material-ui/core/IconButton';
 import 'rc-slider/assets/index.css';
 import Slider from 'rc-slider';
 import './Navbar.css';
@@ -10,12 +13,17 @@ class Navbar extends Component {
     super(props);
     this.state = {
       format: 'hex',
+      open: false,
     };
     this.handleChange = this.handleChange.bind(this);
+    this.closeSnackbar = this.closeSnackbar.bind(this);
   }
   handleChange(evt) {
-    this.setState({format: evt.target.value});
+    this.setState({format: evt.target.value, open: true});
     this.props.handleChange(evt.target.value);
+  }
+  closeSnackbar() {
+    this.setState({open: false});
   }
   render() {
     const {level, changeLevel} = this.props;
@@ -42,6 +50,30 @@ class Navbar extends Component {
             <MenuItem value="rgba">RGBA - rgba(255, 255, 255, 1.0)</MenuItem>
           </Select>
         </div>
+        <Snackbar
+          anchorOrigin={{vertical: 'bottom', horizontal: 'left'}}
+          open={this.state.open}
+          autoHideDuration={3000}
+          message={
+            <span id="message-id">
+              Format Changed to {format.toUpperCase()}
+            </span>
+          }
+          ContentProps={{
+            'aria-describedby': 'message-id',
+          }}
+          onClose={this.closeSnackbar}
+          action={[
+            <IconButton
+              onClick={this.closeSnackbar}
+              color="inherit"
+              key="close"
+              aria-label="close"
+            >
+              <CloseIcon />
+            </IconButton>,
+          ]}
+        />
       </header>
     );
   }
