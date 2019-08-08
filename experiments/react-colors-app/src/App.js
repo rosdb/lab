079c1f0b2275083de66845737ecd197f8b383987
Rosaria De Biasio <debiasiorosaria@gmail.com>
2019-08-08 14:40:37 +0200

    add palette list and links

--- experiments/react-colors-app/src/App.js
+++ experiments/react-colors-app/src/App.js
@@ -3,6 +3,7 @@ import {Route, Switch} from 'react-router-dom';
 import Palette from './Palette';
 import seedColors from './seedColors';
 import {generatePalette} from './colorHelpers';
+import PaletteList from './PaletteList';
 
 class App extends Component {
   findPalette(id) {
@@ -11,7 +12,11 @@ class App extends Component {
   render() {
     return (
       <Switch>
-        <Route exact path="/" render={() => <h1>Palette List</h1>} />
+        <Route
+          exact
+          path="/"
+          render={() => <PaletteList palettes={seedColors} />}
+        />
         <Route
           exact
           path="/palette/:id"
