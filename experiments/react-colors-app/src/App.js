3000044234ca6447b200cbdf34aec9d945ebfa51
Rosaria De Biasio <debiasiorosaria@gmail.com>
2019-08-09 10:54:35 +0200

    add single color palette and link

--- experiments/react-colors-app/src/App.js
+++ experiments/react-colors-app/src/App.js
@@ -4,6 +4,7 @@ import Palette from './Palette';
 import seedColors from './seedColors';
 import {generatePalette} from './colorHelpers';
 import PaletteList from './PaletteList';
+import SingleColorPalette from './SingleColorPalette';
 
 class App extends Component {
   findPalette(id) {
@@ -34,7 +35,7 @@ class App extends Component {
         <Route
           exact
           path="/palette/:paletteId/:colorId"
-          render={() => <h1>Single Color Page</h1>}
+          render={() => <SingleColorPalette />}
         />
       </Switch>
     );
