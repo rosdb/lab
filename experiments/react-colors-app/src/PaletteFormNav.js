c816439c35e72d91d4bb179620d9fd8a6ea58cb1
Rosaria De Biasio <debiasiorosaria@gmail.com>
2019-08-14 11:58:01 +0200

    start to style palette form navbar

--- experiments/react-colors-app/src/PaletteFormNav.js
+++ experiments/react-colors-app/src/PaletteFormNav.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import {makeStyles} from '@material-ui/core/styles';
 import clsx from 'clsx';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import AppBar from '@material-ui/core/AppBar';
@@ -10,8 +11,41 @@ import MenuIcon from '@material-ui/icons/Menu';
 import Button from '@material-ui/core/Button';
 import {ValidatorForm, TextValidator} from 'react-material-ui-form-validator';
 
+const drawerWidth = 400;
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+  },
+  appBar: {
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    height: '64px',
+  },
+  appBarShift: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: drawerWidth,
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  hide: {
+    display: 'none',
+  },
+  navBtns: {},
+}));
+
 export default function PaletteFormNav(props) {
-  const {classes, open, palettes, handleDrawerOpen, handleSubmit} = props;
+  const classes = useStyles();
+  const {open, palettes, handleDrawerOpen, handleSubmit} = props;
   const [newPaletteName, setPaletteName] = React.useState('');
 
   React.useEffect(() => {
@@ -27,7 +61,7 @@ export default function PaletteFormNav(props) {
   }
 
   return (
-    <div>
+    <div className={classes.root}>
       <CssBaseline />
       <AppBar
         position="fixed"
@@ -47,9 +81,11 @@ export default function PaletteFormNav(props) {
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" noWrap>
-            Persistent drawer
+            Create a Palette
           </Typography>
-          <ValidatorForm onSubmit={() => handleSubmit(newPaletteName)}>
+        </Toolbar>
+        <div className={classes.navBtns}>
+          {/* <ValidatorForm onSubmit={() => handleSubmit(newPaletteName)}>
             <TextValidator
               label="Palette Name"
               value={newPaletteName}
@@ -64,13 +100,13 @@ export default function PaletteFormNav(props) {
             <Button variant="contained" color="primary" type="submit">
               Save Palette
             </Button>
-            <Link to="/">
-              <Button variant="contained" color="secondary" type="button">
-                Go Back
-              </Button>
-            </Link>
-          </ValidatorForm>
-        </Toolbar>
+          </ValidatorForm> */}
+          <Link to="/">
+            <Button variant="contained" color="secondary" type="button">
+              Go Back
+            </Button>
+          </Link>
+        </div>
       </AppBar>
     </div>
   );
